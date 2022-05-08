f200536a3c8cfe7e2a257f96324d628862a079be
Alison Tinker <tinker.alison@gmail.com>
2022-05-08 19:45:00 -0400

    update dice command

--- commands/dice.js
+++ commands/dice.js
@@ -1,10 +1,26 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const calc = require('../util/calc');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('dice')
-		.setDescription('Let\'s roll!'),
+		.setDescription('Let\'s roll!')
+		.addIntegerOption(option =>
+			option.setName('rolls')
+				.setDescription('How many rolls?'))
+		.addIntegerOption(option =>
+			option.setName('die')
+				.setDescription('Pick a die')
+				.setRequired(true)
+				.addChoice('d20', 20)
+				.addChoice('d6', 6))
+		.addIntegerOption(option =>
+			option.setName('modifier')
+				.setDescription('Add modifier')),
 	async execute(interaction) {
-		await interaction.reply(`hey ${interaction.user.username}, wanna roll a d20?`);
+		const dieChoice = interaction.options.getInteger('die');
+		const rollResult = calc.getRandomIntInclusive(1, dieChoice);
+
+		await interaction.reply(`${interaction.user.username} rolled **d${dieChoice}** and got **${rollResult}**`);
 	},
 };
---
+++ util/calc.js
@@ -0,0 +1,8 @@
+// Calculations
+
+exports.getRandomIntInclusive = function(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1) + min);
+	// Maximum and minimum are inclusive
+};
